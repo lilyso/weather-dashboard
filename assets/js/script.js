51a7295860fd22df9cd024cab4a95c4dc9008373
lilys0 <lsouryaphanh@gmail.com>
2021-09-24 18:41:11 +1000

    page set up

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// 1. page loads
+//     - time and date displayed
+//         -moment()
+//     - local weather conditions displayed (shoalhaven)
+//         1. API link + API key
+//         - 5 day forecast cards
+//             - temp - *c
+//             - wind speed - kms/hr
+//             - humidity - %
+//             - icon
+//             - colour indication of favourability
+// 2. form loads with past history
+//         displays same as above
+// unit of measurement  (&units=metric)
+// temp - celcius
+// wind - km/hr
+// api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335    &units=metric
+// icons include
+// - sunshine
+// - sun/cloud
+// - cloud
+// - rain
+// - storm
+// - storm/lightning
+// - windy
