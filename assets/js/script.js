d52f9a5784d954d3d235ee15750b048f31cac784
lilys0 <lsouryaphanh@gmail.com>
2021-09-25 00:42:47 +1000

    fetch api & forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,67 @@
-// 1. page loads
-//     - time and date displayed
-//         -moment()
-//     - local weather conditions displayed (shoalhaven)
-//         1. API link + API key
-//         - 5 day forecast cards
-//             - temp - *c
-//             - wind speed - kms/hr
-//             - humidity - %
-//             - icon
-//             - colour indication of favourability
-// 2. form loads with past history
-//         displays same as above
-// unit of measurement  (&units=metric)
-// temp - celcius
-// wind - km/hr
-// api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335    &units=metric
-// icons include
-// - sunshine
-// - sun/cloud
-// - cloud
-// - rain
-// - storm
-// - storm/lightning
-// - windy
+//  API link + API key
+//  Time and date displayed
+//     - moment()
+//  Local weather
+//     - temp, wind, humidity, uv index with colour rating
+//  5 day forecast cards
+//     - temp - *c
+//     - wind speed - kms/hr
+//     - humidity - %
+//     - UV index & colour rating
+//     - icon
+//  Form loads with past history
+//     - button elements
+//     - local weather
+//     - local storage
+//  API
+//     api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335    &units=metric
+//  Icons
+//     - sunshine
+//     - sun/cloud
+//     - cloud
+//     - rain
+//     - storm
+//     - storm/lightning
+//     - windy
+
+// Current date
+document.querySelector("#current-date").textContent =
+  moment().format("DD/MM/YYYY");
+
+document.querySelector("#day2").textContent = moment()
+  .add(1, "day")
+  .format("DD/MM/YYYY");
+
+var apiKey = "65f855456871827b60b4fe75048ff499";
+var userInput = document.querySelector("#user-input");
+var formEl = document.querySelector("#search-form");
+var currentCity = document.querySelector("#current-city");
+var tempVal = document.querySelector("#temp");
+var windVal = document.querySelector("#wind");
+var humidVal = document.querySelector("#humid");
+var uviVal = document.querySelector("#uvi");
+
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (userInput.value) {
+    getWeather();
+  }
+});
+
+async function getWeather() {
+  //   var fivedayforecast = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/forecast?q=${userInput.value}&appid=${apiKey}&units=metric`
+  //   ).then((response) => response.json());
+  //   console.log(fivedayforecast);
+
+  var oneCall = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${fivedayforecast.city.coord.lat}&lon=${fivedayforecast.city.coord.lon}&appid=${apiKey}&units=metric`
+  ).then((response) => response.json());
+  console.log(oneCall);
+  var displayCity = userInput.value.toUpperCase();
+  currentCity.textContent = displayCity;
+  tempVal.textContent = oneCall.current.temp;
+  windVal.textContent = oneCall.current.wind_speed;
+  humidVal.textContent = oneCall.current.humidity;
+  uviVal.textContent = oneCall.current.uvi;
+}
